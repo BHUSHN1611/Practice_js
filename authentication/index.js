0848a4a15e5021fc85fe8c69d8540deb49508d64
BHUSHN1611 <bhushanjadhav1603@gmail.com>
2026-06-23 20:05:57 +0530

    feat: implement user signup and login functionality with JWT authentication

--- authentication/index.js
+++ authentication/index.js
@@ -2,11 +2,43 @@ import express from "express"
 import mongoose from "mongoose"
 import jwt from "jsonwebtoken"
 
-const mongoDburl = "mongodb+srv://bhushanjadhav1603_db_user:bhushanjadhav1603_db_user@cluster0.eteefvy.mongodb.net/?appName=Cluster0"
-
 const app = express()
 const port = 4000
+const jwtpassword = "123456"
 
 app.use(express.json())
+
+mongoose.connect("mongodb+srv://bhushan:Bhushan123@cluster0.k3daqiw.mongodb.net/")
+// CRUD 
+// Create , update ,delete , rename 
+
+const User = mongoose.model('Users',{name: String,email:String,password:String})
+
+app.post("/signup",async (req,res)=>{
+    const {username, email , password} = req.body
+
+    const existingUser = await User.findOne({email:email})
+    
+    if(!existingUser){
+        const user = new User({name:username,password,email})
+        user.save()
+        var token = jwt.sign({username:username},jwtpassword)
+        return res.status(200).json({msg:"User sign in successfully",token})
+    }
+    return res.status(401).json({msg:"User already exists"})
+    
+})
+
+app.get("/login",(req,res)=>{
+    const token = req.headers.authorization
+    const decodedToken = jwt.verify(token,jwtpassword)
+    if(decodedToken){
+        return res.json({msg:"Hey",username:decodedToken.username})
+    }
+    return res.json({msg:"Invalid token"})
+})
+
+
+
 
 app.listen(port)
